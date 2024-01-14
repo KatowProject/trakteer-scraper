e7b47e6adf56d6b6359380e2f1c8529942fac023
KatowProject <naufalfaqih443@gmail.com>
2024-01-14 14:16:21 +0700

    partial update

--- src/tools.ts
+++ src/tools.ts
@@ -1,21 +1,35 @@
 import axios, { AxiosInstance, AxiosResponse } from "axios";
+import { HttpProxyAgent } from "http-proxy-agent";
 
 class AxiosRequest {
     public request: AxiosInstance;
     public self = axios;
     private BASE_URL: string = "https://trakteer.id/";
 
-    constructor(auth: { XSRF_TOKEN: String, TRAKTEER_SESSION: String }) {
-        this.request = axios.create({
-            baseURL: this.BASE_URL,
-            timeout: 10000,
-            headers: {
-                "User-Agent": "Mozilla/5.0 (Linux; Android 10; Redmi Note 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Mobile Safari/537.36",
-                "Accept": "application/json, text/plain, */*",
-                "Referer": "https://trakteer.id/",
-                "cookie": `XSRF-TOKEN=${auth.XSRF_TOKEN}; trakteer-sess=${auth.TRAKTEER_SESSION}`
-            }
-        });
+    constructor(auth: { XSRF_TOKEN: String, TRAKTEER_SESSION: String }, proxy?: string) {
+        if (proxy)
+            this.request = axios.create({
+                baseURL: this.BASE_URL,
+                timeout: 10000,
+                headers: {
+                    "User-Agent": "Mozilla/5.0 (Linux; Android 10; Redmi Note 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Mobile Safari/537.36",
+                    "Accept": "application/json, text/plain, */*",
+                    "Referer": "https://trakteer.id/",
+                    "cookie": `XSRF-TOKEN=${auth.XSRF_TOKEN}; trakteer-sess=${auth.TRAKTEER_SESSION}`
+                },
+                httpAgent: new HttpProxyAgent(proxy)
+            });
+        else
+            this.request = axios.create({
+                baseURL: this.BASE_URL,
+                timeout: 10000,
+                headers: {
+                    "User-Agent": "Mozilla/5.0 (Linux; Android 10; Redmi Note 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Mobile Safari/537.36",
+                    "Accept": "application/json, text/plain, */*",
+                    "Referer": "https://trakteer.id/",
+                    "cookie": `XSRF-TOKEN=${auth.XSRF_TOKEN}; trakteer-sess=${auth.TRAKTEER_SESSION}`
+                }
+            });
     }
 
     get(endpoint: string, params: Object = {}): Promise<AxiosResponse | undefined> {
